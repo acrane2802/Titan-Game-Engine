6845078b465b1058964520df973ea43d91b1a497
Andrew Crane <andrew.crane2802@gmail.com>
2023-11-25 17:02:45 -0600

    intial commit

---
+++ Engine/src/Engine.cpp
@@ -0,0 +1,7 @@
+﻿#include "Engine.h"
+
+int __declspec(dllexport) main()
+{
+	std::cout << "Hello Engine." << std::endl;
+	return 0;
+}
---
+++ Game/src/Game.cpp
@@ -0,0 +1,7 @@
+#include "Game.h"
+
+int main()
+{
+	std::cout << "Hello Game." << std::endl;
+	return 0;
+}
